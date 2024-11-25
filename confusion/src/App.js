e2e50e3b88843e4ef37bd77ee21ff26c96b3e668
Pedro Leon <leon@gmail.com>
2024-11-25 20:20:43 -0300

    Components Part 1

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -1,21 +1,17 @@
-
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
 
 function App() {
   return (
     <div className="App">
       <Navbar dark color="primary">
-
         <div className="container">
-
           <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-
-        <div>Student:Pedro Leon</div>
-
-    </div>
-
-        </Navbar>
+          <div>Student: Pedro Leon</div>
+        </div>
+      </Navbar>
+      <Menu />
     </div>
   );
 }
